35fced766e462005caee51bb7205c32ae9361547
Maryan Abey Ali <maryamabey@gmail.com>
2025-09-20 13:41:22 +0200

    Conflict resolved

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Ændring fra VSCode, som skaber konflikt!");
-    }
 public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Ændring fra IntelliJ, som skaber konflikt!");
+    System.out.println("Ændring fra VSCode, som skaber konflikt!");
+
+    {
+      System.out.println("Ændring fra IntelliJ, som skaber konflikt!");
+    }
   }
 }
+
